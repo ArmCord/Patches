25fa875e10801681b7bd655e2868622de3b797f7
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2022-12-03 12:48:36 +0100

    Update titlebar fix

--- 3.1.0/patch.js
+++ 3.1.0/patch.js
@@ -1,6 +1,7 @@
- console.log("Injecting titlebar fix")
- var elem = document.createElement("div");
-        elem.innerHTML = `<nav class="titlebar">
+if (document.getElementById("window-controls-container") == null) {
+  console.log("Injecting titlebar fix");
+  var elem = document.createElement("div");
+  elem.innerHTML = `<nav class="titlebar">
           <div class="window-title" id="window-title"></div>
           <div id="window-controls-container">
               <div id="spacer"></div>
@@ -9,9 +10,25 @@
               <div id="quit"><div id="quit-icon"></div></div>
           </div>
         </nav>`;
-        elem.classList.add("withFrame-haYltI");
-        if (document.getElementById("app-mount") == null) {
-            document.body.appendChild(elem);
-        } else {
-            document.getElementById("app-mount").prepend(elem);
-        }
+  elem.classList.add("withFrame-haYltI");
+  if (document.getElementById("app-mount") == null) {
+    document.body.appendChild(elem);
+  } else {
+    document.getElementById("app-mount").prepend(elem);
+  }
+  var minimize = document.getElementById("minimize");
+  var maximize = document.getElementById("maximize");
+  var quit = document.getElementById("quit");
+
+  minimize.addEventListener("click", () => {
+    armcord.window.minimize();
+  });
+
+  maximize.addEventListener("click", () => {
+    armcord.window.maximize();
+  });
+
+  quit.addEventListener("click", () => {
+    armcord.window.hide();
+  });
+}
