bd70383b1ced04091f047109e693f804901c61d6
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2022-12-03 12:41:23 +0100

    Add titlebar fix

--- 3.1.0/patch.js
+++ 3.1.0/patch.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+ var elem = document.createElement("div");
+        elem.innerHTML = `<nav class="titlebar">
+          <div class="window-title" id="window-title"></div>
+          <div id="window-controls-container">
+              <div id="spacer"></div>
+              <div id="minimize"><div id="minimize-icon"></div></div>
+              <div id="maximize"><div id="maximize-icon"></div></div>
+              <div id="quit"><div id="quit-icon"></div></div>
+          </div>
+        </nav>`;
+        elem.classList.add("withFrame-haYltI");
+        if (document.getElementById("app-mount") == null) {
+            document.body.appendChild(elem);
+        } else {
+            document.getElementById("app-mount").prepend(elem);
+        }
